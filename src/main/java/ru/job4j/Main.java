667df06c16e78c64f05774509e794edbb0581bb2
kirillkrohmal <krohmal_kirill@mail.ru>
2020-04-08 14:57:34 +0300

    cinemahalls3

--- src/main/java/ru/job4j/Main.java
+++ src/main/java/ru/job4j/Main.java
@@ -7,7 +7,7 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
 
-        /*List<Place> places = new ArrayList<>();
+        List<Place> places = new ArrayList<>();
         int width_and_height=10;
 
         Place place1 = new Place("0", "0 ");
@@ -37,6 +37,8 @@ public class Main {
         places.add(place11);
         places.add(place12);
 
+        /*
+
         for(int i=0;i<width_and_height ; i++) {
             System.out.println("+----+----+----+----+----+----+----+----+----+----+");
 
@@ -56,30 +58,7 @@ public class Main {
         }
 */
 
-        String html = "<<!DOCTYPE html>\n" +
-                "<html lang=\"en\" xmlns:c=\"\">\n" +
-                "<head>\n" +
-                "    <!-- Required meta tags -->\n" +
-                "    <meta charset=\"utf-8\">\n" +
-                "    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1, shrink-to-fit=no\">\n" +
-                "\n" +
-                "    <!-- Bootstrap CSS -->\n" +
-                "    <link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css\" integrity=\"sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm\" crossorigin=\"anonymous\">\n" +
-                "    <script src=\"https://code.jquery.com/jquery-3.2.1.slim.min.js\" integrity=\"sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN\" crossorigin=\"anonymous\"></script>\n" +
-                "    <script src=\"https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js\" integrity=\"sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q\" crossorigin=\"anonymous\"></script>\n" +
-                "    <script src=\"https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js\" integrity=\"sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl\" crossorigin=\"anonymous\"></script>\n" +
-                "\n" +
-                "    <title>Сервис - Кинотеатр</title>\n" +
-                "</head>\n" +
-                "\n" +
-                "\n" +
-                "<script src=\"https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js\"></script>\n" +
-                "<div class=\"container\">\n" +
-                "    <div class=\"row pt-3\">\n" +
-                "        <h4>\n" +
-                "            Бронирование месте на сеанс\n" +
-                "        </h4>\n" +
-                "        <table class=\"table table-bordered\">\n" +
+        String html = "\"<table class=\"table table-bordered\">\n" +
                 "            <thead>\n" +
                 "            <tr>\n" +
                 "                <th style=\"width: 120px;\">Ряд / Место</th>\n" +
@@ -91,31 +70,9 @@ public class Main {
                 "            <tbody>\n" +
                 "            <tr>\n" +
                 "                <th>1</th>\n" +
-                "                <td><input type=\"radio\" name=\"place\" value=\"11\"> занято место. Ряд 1, Место 1</td>\n" +
-                "                <td><input type=\"radio\" name=\"place\" value=\"11\"> занято место. Ряд 1, Место 2</td>\n" +
-                "                <td><input type=\"radio\" name=\"place\" value=\"11\"> Ряд 1, Место 3</td>\n" +
-                "            </tr>\n" +
-                "            <tr>\n" +
-                "                <th>2</th>\n" +
-                "                <td><input type=\"radio\" name=\"place\" value=\"11\"> занято место. Ряд 2, Место 1</td>\n" +
-                "                <td><input type=\"radio\" name=\"place\" value=\"11\"> Ряд 2, Место 2</td>\n" +
-                "                <td><input type=\"radio\" name=\"place\" value=\"11\"> Ряд 2, Место 3</td>\n" +
-                "            </tr>\n" +
-                "            <tr>\n" +
-                "                <th>3</th>\n" +
-                "                <td><input type=\"radio\" name=\"place\" value=\"11\"> Ряд 3, Место 1</td>\n" +
-                "                <td><input type=\"radio\" name=\"place\" value=\"11\"> Ряд 3, Место 2</td>\n" +
-                "                <td><input type=\"radio\" name=\"place\" value=\"11\"> занято место. Ряд 3, Место 3</td>\n" +
-                "            </tr>\n" +
-                "            </tbody>\n" +
-                "        </table>\n" +
-                "    </div>\n" +
-                "    <div class=\"row float-right\">\n" +
-                "        <button type=\"button\" class=\"btn btn-success\" onclick=\"addRowAndPlace()\">Оплатить</button>\n" +
-                "    </div>\n" +
-                "</div>\n" +
-                "</body>\n" +
-                "</html>";
+                "                <td><input type=\"radio\" name=\"place\" value=\"11\">" + place1 + " занято <td>" +
+                "                <td><input type=\"radio\" name=\"place\" value=\"11\">" + place7 + " свободно <td>" +
+                "                <td><input type=\"radio\" name=\"place\" value=\"11\">" + place8 + " занято <td>";
 
 
         System.out.println(html);
